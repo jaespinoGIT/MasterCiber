429f70fbebbe939f6f55ebced2e6d199c21c8cb2
jaespino <jaespino@inerza.com>
2019-10-04 14:40:50 +0100

    Añade ejemplo tripledes

--- M1T1/M1TripleDES.py
+++ M1T1/M1TripleDES.py
@@ -1,8 +1,13 @@
+import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 backend = default_backend()
-key=bytes(str('123455789012345678901234'), 'ascii')
+key = os.urandom(24)
 cipher = Cipher(algorithms.TripleDES(key), modes.ECB(), backend=backend)
 encryptor = cipher.encryptor()
 cryptogram = encryptor.update(b"a secret message") + encryptor.finalize()
 print(cryptogram)
+
+decryptor = cipher.decryptor()
+clearText=decryptor.update(cryptogram) + decryptor.finalize()
+print(clearText)
